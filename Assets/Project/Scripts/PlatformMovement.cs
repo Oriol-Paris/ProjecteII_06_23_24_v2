b21220cb42f61584560a2ad390303ca23181ec2c
Eric-Maestre <eric.maestre@enti.cat>
2024-02-09 20:16:51 +0100

    platform Movement Refined,
    level till 12

--- Assets/Project/Scripts/PlatformMovement.cs
+++ Assets/Project/Scripts/PlatformMovement.cs
@@ -4,104 +4,62 @@ using UnityEngine;
 
 public class PlatformMovement : MonoBehaviour
 {
-    [SerializeField]
-    private bool firstLeft; // Direcci�n inicial hacia la izquierda
-    [SerializeField]
-    private bool firstUp; //Direccionj inicial hacia arriba
-    [SerializeField]
-    private bool centered;
-    [SerializeField]
-    private bool bounce; // Rebotar con ciertos elementos
-    [SerializeField]
-    private bool allBounce; // Rebotar con todo
-    [SerializeField]
-    private bool upDown;
 
     [SerializeField]
-    private float range;
-    [SerializeField]
-    private float velocity;
+    private float velocity; //unitsPerSec
 
     private Vector2 initialPosition;
-    private Vector2 targetPosition;
+
+    [SerializeField]
+    private List<Vector2> targetPoints;
+
+    [SerializeField]
+    private AnimationCurve movementCurve;
+    private float currentLerpTime = 0.0f;
+    private float lerpTime = 0.0f;
+    private int currentIndex = 0;
 
     private Rigidbody2D rb;
 
     void Start()
     {
-        if (bounce || allBounce)
-            BounceMovement();
         initialPosition = transform.position;
-        CalculateTargetPosition();
+        for(int i = 0; i < targetPoints.Count; i++)
+        {
+            targetPoints[i] += (Vector2)transform.position;
+        }
+        targetPoints.Insert(0, initialPosition);
+        lerpTime = Vector2.Distance(targetPoints[0], targetPoints[1]) / velocity;
     }
 
 
     void Update()
     {
-        if (bounce || allBounce)
+        currentLerpTime = Mathf.Min(currentLerpTime + Time.deltaTime, lerpTime);
+
+        float t = currentLerpTime / lerpTime;
+
+        this.transform.position = Vector2.Lerp(targetPoints[currentIndex],
+            targetPoints[(currentIndex + 1) % targetPoints.Count],
+            movementCurve.Evaluate(t));
+
+        if(currentLerpTime == lerpTime)
         {
-            rb = GetComponent<Rigidbody2D>();
-            if (rb == null)
-            {
-                Debug.LogError("Error");
-            }
-            BounceMovement();
-            return;
+            int temp = currentIndex;
+            currentIndex = (currentIndex + 1) % targetPoints.Count;
+            lerpTime = Vector2.Distance(targetPoints[temp], targetPoints[currentIndex]) / velocity;
+            currentLerpTime = 0.0f;
         }
-        float t = Mathf.PingPong(Time.time * velocity, 1.0f);
-        transform.position = Vector3.Lerp(initialPosition, targetPosition, t);
     }
 
-    private void BounceMovement()
+
+    private void OnDrawGizmosSelected()
     {
-        float directionX = firstLeft ? -1.0f : 1.0f;
-        float directionY = firstUp ? 1.0f : -1.0f;
-
-        Vector2 direction = new Vector2(directionX, directionY);
-
-        rb.AddForce(direction * velocity);       
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        //switch (allBounce)
-        //{
-        //    case true:
-        //        //loogic
-        //        return;
-        //        break;
-        //    case false:
-        //        switch (bounce)
-        //        {
-        //            case true:
-        //                if (collision.gameObject.CompareTag("Player"))
-        //                    return;
-
-        //                return;
-        //                break;
-        //            case false:
-        //                return;
-        //                break;
-        //        }
-        //        break;
-        //}
-    }
-
-    private void CalculateTargetPosition()
-    {
-        float halfRange = centered ? range / 2.0f : 0.0f;
-        float directionX = firstLeft ? -1.0f : 1.0f;
-        float directionY = firstUp ? 1.0f : -1.0f;
-
-        if (upDown)
-        {
-            initialPosition = new Vector2(initialPosition.x, initialPosition.y - halfRange);
-            targetPosition = new Vector2(initialPosition.x, initialPosition.y + range * directionY);
+        Gizmos.DrawSphere(this.transform.position, 0.2f);
+        foreach (Vector2 v in targetPoints) {
+            Gizmos.DrawSphere(this.transform.position + (Vector3)v, 0.2f);
         }
-        else
-        {
-            initialPosition = new Vector2(initialPosition.x - halfRange * directionX, initialPosition.y);
-            targetPosition = new Vector2(initialPosition.x + range * directionX, initialPosition.y);
-        }
+
+        
     }
 }
